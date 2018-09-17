c8b0e9faf0588018177ca9608ba09be0ac898070
BalasundaramVi <vigneshbalasundaram@gmail.com>
2018-09-17 11:28:27 -0700

    added to stringifyJSON 'array'

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,20 +4,42 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
+  if (obj === null) {
+    return ('null');
+  }
+
+  if (obj === undefined) {
+    return ('undefined');
+  }
+  
   if (typeof obj === 'number') {
     return obj.toString();
-  };
+  }
 
   if (typeof obj === 'string') {
     return obj;
-  };
+  }
 
   if (typeof obj === 'boolean') {
     if (obj === true) {
-      return('true');
+      return ('true');
     } else {
-      return('false');
+      return ('false');
     }
   }
+  
+  if (Array.isArray(obj)) {
+    var outputObj = '[';
+    for (var i = 0; i < obj.length; i++) {
+      outputObj += stringifyJSON(obj[i]) + ', ';
+    }
+    outputObj.splice(0, outputObj.length-2);
+    outputObj += ']';
+    return outputObj;
+  }
+
+  if (typeof obj === 'object') {
+  
+  }
+
 };
-s
