790daaba0f854638a61fe79580a7072747e265e2
BalasundaramVi <vigneshbalasundaram@gmail.com>
2018-09-17 12:27:03 -0700

    got getElementsByClassName to pass all bare minimum conditions

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,31 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className, node) {
+  if (node === undefined) {
+    node = document.body;
+  }
+  
+  if (node === null) {
+    return;
+  }
+
+  if (node.classList === undefined) {
+    return;
+  }
+
+  var output = [];
+
+  if (node.classList.contains(className)) {
+    output.push(node);
+  }
+  for (var i = 0; i < node.childNodes.length; i++) {
+    var child = node.childNodes[i];
+
+    var children = getElementsByClassName(className, child);
+    if (children !== undefined) {
+      output = output.concat(children);
+    }
+  }
+  return (output);
 };
