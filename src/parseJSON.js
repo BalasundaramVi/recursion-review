dd587f810560a257ce872831985f241e6778f6cd
BalasundaramVi <vigneshbalasundaram@gmail.com>
2018-09-17 20:11:38 -0700

    small formatting changes (more comments)

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,22 +3,55 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  var index = 0;
-  //isSomething helpers
-  var nextChar = function() {
+  if (json === undefined) {
+    return;
+  }
   
-  };
+  if (isObject(json)) {
+    var object = toObject(json);
+    
+  }
   
   //combineStuff
+  var container = undefined;
+
+  return outputFunction();
 };
 
 
 //   var inputString = json.replace(' ', '');
 
+var outputFunction = function(str) {
+  if (isObject[json]) {
+    container = {};
+  } else if ()
+
+  return outputFunction(substring); 
+};
+
+"""" 3 """"
+
 
 /////////////////////////////////////////////////////////////
 // GRAMMAR HELPER FUNCTIONS
 /////////////////////////////////////////////////////////////
+
+var toObject = function(str) {
+  var quotes = 0;
+  for (var i = 0; i < str.length; i++) {
+    if(str[i] === '"') {
+      quotes++;
+    }
+    if (quotes === 2) {
+      quotes = 0;
+    }
+
+    if (str[i] === ':' && quotes !== 1) {
+      
+    }
+  }
+
+};
 
 var isObject = function(str) {
   
@@ -37,11 +70,17 @@ var isArray = function(str) {
 };
 
 var isElements = function(str) {
+  var output = undefined;
+  if (isValue(str)) {
+    return true;
+  }
 
+
+  return false;
 };
 
 var isValue = function(str) {
-
+  return (isNumber(str) || isString(str) || isBoolean(str) || isNull(str));
 };
 
 /////////////////////////////////////////////////////////////
